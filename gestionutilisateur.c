a1b2285244dde9b155a3818da576bd6f75b84d65
feriel-b <feriel.bouguecha@esprit.tn>
2022-11-07 10:49:55 +0100

    update gest util

--- gestionutilisateur.c
+++ gestionutilisateur.c
@@ -1,7 +1,102 @@
 #include "gestionutilisateur.h"
 
 
-
-void gestionUtilisateur(){
-    
+int ajouter(char * filename, user u )
+{
+    FILE * f=fopen(filename, "a");
+    if(f!=NULL)
+    {
+        fprintf( f,"%s %s %d %d %d %d %s %s %s %s \n", p.prenom ,p.nom ,p.cin ,p.dateNaiss.j ,p.dateNaiss.m ,p.dateNaiss.a ,p.id ,p.md,p.bv, p.role );
+        fclose(f);
+        return 1;
+    }
+    else return 0;
 }
+
+
+char prenom [20];
+    char nom [20];
+    int cin;
+    date dateNaiss;
+    char id [20];
+    char mdp [20];
+    char bv [20];
+    char role [20];
+
+
+
+
+int modifier( char * filename, int id, user nouv )
+{
+    int tr=0;
+     ;
+    FILE * f=fopen(filename, "r");
+    FILE * f2=fopen("nouv.txt", "w");
+    if(f!=NULL && f2!=NULL)
+    {
+        while(fscanf(f,"%s %s %d %d %d %d %s %s %s %s \n",
+         &p.prenom ,&p.nom ,&p.cin ,&p.dateNaiss.j ,&p.dateNaiss.m ,&p.dateNaiss.a ,&p.id ,&p.md,&p.bv, &p.role)!=EOF)
+        {
+            if(p.id== id)
+            {
+                fprintf(f2,"%s %s %d %d %d %d %s %s %s %s \n",
+         nouv.prenom ,nouv.nom ,nouv.cin ,nouv.dateNaiss.j ,nouv.dateNaiss.m ,nouv.dateNaiss.a ,nouv.id ,nouv.md,nouv.bv, nouv.role);
+                tr=1;
+            }
+            else
+                fprintf(f2,"%s %s %d %d %d %d %s %s %s %s \n", p.prenom ,p.nom ,p.cin ,p.dateNaiss.j ,p.dateNaiss.m ,p.dateNaiss.a ,p.id ,p.md,p.bv, p.role );
+        
+
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove(filename);
+    rename("nouv.txt", filename);
+    return tr;
+
+}
+int supprimer(char * filename, int id)
+{
+    int tr=0;
+     ;
+    FILE * f=fopen(filename, "r");
+    FILE * f2=fopen("nouv.txt", "w");
+    if(f!=NULL && f2!=NULL)
+    {
+        while(fscanf(f,"%s %s %d %d %d %d %s %s %s %s \n",
+         &p.prenom ,&p.nom ,&p.cin ,&p.dateNaiss.j ,&p.dateNaiss.m ,&p.dateNaiss.a ,&p.id ,&p.md,&p.bv, &p.role)!=EOF)
+        {
+            if(p.id== id)
+                tr=1;
+            else
+                fprintf(f2,"%s %s %d %d %d %d %s %s %s %s \n", p.prenom ,p.nom ,p.cin ,p.dateNaiss.j ,p.dateNaiss.m ,p.dateNaiss.a ,p.id ,p.md,p.bv, p.role );
+        }
+    }
+    fclose(f);
+    fclose(f2);
+    remove(filename);
+    rename("nouv.txt", filename);
+    return tr;
+}
+ user chercher(char * filename, int id)
+{
+     ;
+    int tr;
+    FILE * f=fopen(filename, "r");
+    if(f!=NULL)
+    {
+        while(tr==0&& fscanf(f,"%s %s %d %d %d %d %s %s %s %s \n",
+         &p.prenom ,&p.nom ,&p.cin ,&p.dateNaiss.j ,&p.dateNaiss.m ,&p.dateNaiss.a ,&p.id ,&p.md,&p.bv, &p.role)!=EOF)
+        {
+            if(p.id== id)
+                tr=1;
+        }
+    }
+    fclose(f);
+    if(tr==0)
+        p.id=-1;
+    return p;
+
+}
+
--- gestionutilisateur.h
+++ gestionutilisateur.h
@@ -1,14 +1,27 @@
 #ifndef GESTIONUTILISATEUR_H_INCLUDED
 #define GESTIONUTILISATEUR_H_INCLUDED
 #include <stdio.h>
-typedef struct
+
+typedef struct Date
 {
-    char couleur [20];
-    int id, x,y;
-} point;
+    int j,m,a;
+
+} date;
+
+typedef struct Utilsateur
+{
+    char prenom [20];
+    char nom [20];
+    int cin;
+    date dateNaiss;
+    char id [20];
+    char mdp [20];
+    char bv [20];
+    char role [20];
+}user ;
 //le type de  retour int c'est pour indiquer si la tache a été réalisée avec succés ou non// pour afficher des lsg plus tard exemple erreur d'ouverture de fichier, element introuvable
-int ajouter(char *, point );
-int modifier( char *, int, point );
+int ajouter(char *, user );
+int modifier( char *, int, user );
 int supprimer(char *, int );
 point chercher(char *, int);
 
