8cbf0ae23d7f77a34f917faee8250b09e22b40bf
feriel-b <feriel.bouguecha@esprit.tn>
2022-11-14 11:02:27 +0100

    ajout fichier

---
+++ mainUtil.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include"gestionutilisateur.h"
+
+int main()
+{
+    user us={"feriel","bg",14,26,07,2002,"id","mdp","bv1","electeur"};
+    user us1={"fe","bc",34,26,07,2003,"id1","mdp1","bv1","electeur"};
+    user us2={"f","b",12,26,07,2002,"id2","mdp2","bv1","agent"};
+
+    int x=ajouter("user.txt", us);
+    int r=ajouter("user.txt", us2);
+
+    printf("hello world");
+    
+    if(x==1)
+        printf("\n ajout de point avec succÈs");
+    else printf("\n echec ajout");
+        
+
+    int v = modifier("user.txt",14,us2 );
+
+    if(v==1)
+        printf("\n Modification de point avec succes");
+    else printf("\n echec Modification");
+
+
+
+    x=supprimer("user.txt",34 );
+    if(x==1)
+        printf("\n Suppression de point avec succÈs");
+    else printf("\n echec Suppression");
+
+    user c=chercher("user.txt",12 );
+    if(strcmp(us2.id ,"-1")== 0)
+        printf("introuvable");
+    return 0;
+}
---
+++ statFeriel.h
@@ -0,0 +1,11 @@
+#ifndef STATFERIEL_H_INCLUDED
+#define STATFERIEL_H_INCLUDED
+#include <stdio.h>
+#include <string.h>
+#include <gestionutilisateur.h>
+
+void TPHF( char * filename, int * f, int *h);
+float agemoyen(char * fichierusers);
+
+
+#endif // STATFERIEL_H_INCLUDED
