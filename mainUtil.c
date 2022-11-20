a4b55637f51720ee04d003bc587c9e9dced51855
feriel-b <feriel.bouguecha@esprit.tn>
2022-11-20 16:37:03 +0100

    m

--- mainUtil.c
+++ mainUtil.c
@@ -8,8 +8,8 @@ int main()
     user us1={"fe","bc",34,26,07,2003,"id1","mdp1","bv1","electeur"};
     user us2={"f","b",12,26,07,2002,"id2","mdp2","bv1","agent"};
 
-    int x=ajouter("user.txt", us);
-    int r=ajouter("user.txt", us2);
+    int x=ajouter("allusers.txt", us);
+    int r=ajouter("allusers.txt", us2);
 
     printf("hello world");
     
@@ -18,7 +18,7 @@ int main()
     else printf("\n echec ajout");
         
 
-    int v = modifier("user.txt",14,us2 );
+    int v = modifier("allusers.txt",14,us2 );
 
     if(v==1)
         printf("\n Modification de point avec succes");
@@ -26,12 +26,12 @@ int main()
 
 
 
-    x=supprimer("user.txt",34 );
+    x=supprimer("allusers.txt",34 );
     if(x==1)
         printf("\n Suppression de point avec succÈs");
     else printf("\n echec Suppression");
 
-    user c=chercher("user.txt",12 );
+    user c=chercher("allusers.txt",12 );
     if(strcmp(us2.id ,"-1")== 0)
         printf("introuvable");
     return 0;
--- statFeriel.c
+++ statFeriel.c
@@ -22,4 +22,19 @@ float TPE(char * filename)
     return(nv/ne);
 }
 
-float agemoyen(char * fichierusers){}
+float agemoyen(char * fichierusers){
+     int nv=0, ne=0;
+    user p;
+    FILE * f=fopen(filename, "r");
+    if(f!=NULL)
+    {
+        while(fscanf(f,"%s %s %s %d %d %d %d %s %s %s %s %d \n",
+         p.prenom ,p.nom ,&p.genre ,&p.cin ,&p.dateNaiss.j ,&p.dateNaiss.m ,&p.dateNaiss.a ,p.id ,p.mdp,p.bv, p.role, &p.vote)!=EOF)
+        {
+            ne++;
+            if(p.vote!=-1)
+                nv++;
+        }
+    }
+    return(nv/ne);
+}
