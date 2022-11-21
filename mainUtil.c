23eda10fefbd7cee9594baa616ceaa597044cca4
feriel-b <feriel.bouguecha@esprit.tn>
2022-11-21 08:58:46 +0100

    m

--- mainUtil.c
+++ mainUtil.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#include"gestionutilisateur.h"
+#include "gestionutilisateur.h"
+#include "statFeriel.h" 
 
 int main()
 {
-    user us={"feriel","bg",14,26,07,2002,"id","mdp","bv1","electeur"};
-    user us1={"fe","bc",34,26,07,2003,"id1","mdp1","bv1","electeur"};
-    user us2={"f","b",12,26,07,2002,"id2","mdp2","bv1","agent"};
+    user us={"feriel","bg",'f',14,26,07,2002,"identif","mdp","bv1","electeur",-1};
+    user us1={"fe","bc",'f',34,26,07,2003,"id1","mdp1","bv1","electeur",0};
+    user us2={"f","b",'f',12,26,07,2002,"id2","mdp2","bv1","agent",2};
 
     int x=ajouter("allusers.txt", us);
     int r=ajouter("allusers.txt", us2);
-
+    int r1= ajouter("allusers.txt", us1);
     printf("hello world");
     
-    if(x==1)
+    if(x==1 && r==1 && r1==1)
         printf("\n ajout de point avec succÈs");
     else printf("\n echec ajout");
         
@@ -31,8 +32,17 @@ int main()
         printf("\n Suppression de point avec succÈs");
     else printf("\n echec Suppression");
 
+    printf("age moy des particip     \n ");
+    float aG = agemoyen("allusers.txt");
+    printf("age moy des particip: %f  \n",aG);
+
+
+
     user c=chercher("allusers.txt",12 );
     if(strcmp(us2.id ,"-1")== 0)
         printf("introuvable");
+    else
+        printf("%s %s %d %d %d %d %s %s %s %s %d %c \n", c.prenom ,c.nom ,c.cin ,c.dateNaiss.j ,c.dateNaiss.m ,c.dateNaiss.a ,c.id ,c.mdp,c.bv, c.role,c.vote,c.genre );
+   
     return 0;
 }
--- statFeriel.c
+++ statFeriel.c
@@ -1,5 +1,5 @@
-#include "statFeriel.h"
 #include "gestionutilisateur.h"
+#include "statFeriel.h"
 
 
 
@@ -22,19 +22,24 @@ float TPE(char * filename)
     return(nv/ne);
 }
 
-float agemoyen(char * fichierusers){
-     int nv=0, ne=0;
+float agemoyen(char * filename){
+    int np=0;//nbr de participants inscrit
+    float aget=0;// somme de ts les ages
+    float anneeCourante=2022;
     user p;
     FILE * f=fopen(filename, "r");
     if(f!=NULL)
     {
         while(fscanf(f,"%s %s %s %d %d %d %d %s %s %s %s %d \n",
          p.prenom ,p.nom ,&p.genre ,&p.cin ,&p.dateNaiss.j ,&p.dateNaiss.m ,&p.dateNaiss.a ,p.id ,p.mdp,p.bv, p.role, &p.vote)!=EOF)
-        {
-            ne++;
-            if(p.vote!=-1)
-                nv++;
+        {   
+            if(p.dateNaiss.a > 0){
+            aget= aget + (anneeCourante- p.dateNaiss.a);
+            np++;
+            }    
+            
+
         }
     }
-    return(nv/ne);
+    return(aget/np);
 }
